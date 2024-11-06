4d6398f9d6fbac0e00f67a4596ea2c1ec93fa395
Milky Lorejo <milkylorejo@gmail.com>
2024-11-06 22:58:37 +0800

    Added exercise 7 and 8

---
+++ 1stsem_24-25/activities/exercise-7-array.py
@@ -0,0 +1,12 @@
+#Create a class named ArrayClass. 
+
+#1.)Inside this class create a function that inserts the following value in an array.
+#77 99 44 55 22 88 11 0 66 33
+
+#2.) Create a function that look for a value in the given array and return an output if not found
+#Can’t find 35
+
+#3.) Delete the following values 0, 55, and 99 in delete function
+#77 44 22 88 11 66 33
+
+#4.) Update the following values 77 and 88 to 55 and 99 in delete function
---
+++ 1stsem_24-25/activities/exercise-8-multi-dimensional-array.py
@@ -0,0 +1,13 @@
+#Create a class named Person. 
+
+#1.) Create a constructor for Percon class that contains lastName, firstName and age
+#2.) Create method displayPerson that prints the person's information.
+#3.) Create method getLast that gets the last name of the person
+
+#Create a class named Person. 
+#1.) Create a class named ClassDataArray that represents an array of Person objects
+#2.) Create the following methods:
+    # find()
+    # insert()
+    # remove()
+    # display()
